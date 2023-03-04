80cbe9e123611c078f951150fad3d3bc72897769
oktaydbk54 <oktaydabak54@hotmail.com>
2023-03-04 16:38:43 +0300

    New version release 2

--- api/iris_logisticreg.py
+++ api/iris_logisticreg.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
+from sklearn.model_selection import train_test_split
 
 # Iris veri setini yükleyelim
 iris_df = pd.read_csv('iris.csv')
 
-# X ve y verilerini ayıralım
-X = iris_df.iloc[:, :-1]
-y = iris_df.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_df.iloc[:, :-1], iris_df.iloc[:, -1], test_size=0.2, random_state=42)
 
 # Modeli eğitelim
 model = LogisticRegression()
-model.fit(X, y)
+model.fit(X_train, y_train)
 
 # Oluşturduğumuz modeli model1.pkl adlı dosyaya kaydedelim
 with open('logistic_regression_model.pkl', 'wb') as f:
